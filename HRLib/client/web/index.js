91f6e5022b8d115690c5561ae08a32ea149de8c7
HRScripts <138999193+HRScripts@users.noreply.github.com>
2024-10-26 11:16:00 +0300

    Delete HRLib directory

--- HRLib/client/web/index.js
+++
@@ -1,6 +0,0 @@
-import 'nui://game/ui/jquery.js';
-
-const modules = [ 'contextMenu', 'textUI', 'notify', 'dialogMenu', 'progressBar' ];
-for (let i = 0; i <= modules.length-1; i++) {
-    import(`./features/${modules[i]}/index.js`);
-};
