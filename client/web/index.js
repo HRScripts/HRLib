d4a8b60a2408ef79fd34b0b0f4d45dc3f8cd900f
HR0110 <hrisiraya@abv.bg>
2024-11-02 19:47:01 +0200

    fix(client/web/index.js): removed a test modules

--- client/web/index.js
+++ client/web/index.js
@@ -1,6 +1,6 @@
 import 'nui://game/ui/jquery.js';
 
-const modules = [ 'contextMenu', 'textUI', 'notify', 'dialogMenu', 'progressBar' ];
+const modules = [ 'textUI', 'notify', 'progressBar' ];
 for (let i = 0; i <= modules.length-1; i++) {
     import(`./features/${modules[i]}/index.js`);
 };
