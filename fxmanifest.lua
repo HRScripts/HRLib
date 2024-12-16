cc1ed8127b605d1afff8173c69123a216da299fa
HR0110 <hrisiraya@abv.bg>
2024-12-16 14:17:14 +0200

    updating version in fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ name 'HRLib'
 author 'HRScripts Development'
 description 'A fivem library script used for all HRScripts Development\'s resources'
 repository 'https://github.com/HRScripts/HRLib'
-version '2.6.0'
+version '2.6.1'
 
 shared_scripts {
     'config.lua',
