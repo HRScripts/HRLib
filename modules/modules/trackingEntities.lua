01bfc26d3ef6b092342b6f06f7bfb2ecd9c2ec55
HR0110 <hrisiraya@abv.bg>
2025-04-13 23:03:16 +0300

    fix(modules/trackingEntities): removed some functions as part of the tracking entities module

--- modules/modules/trackingEntities.lua
+++ modules/modules/trackingEntities.lua
@@ -9,7 +9,11 @@ DeleteEntity = function(entity)
 
     local entityType = GetEntityType(entity)
     entityType = entityType == 1 and 'ped' or entityType == 2 and 'vehicle' or entityType == 3 and 'object' ---@diagnostic disable-line: cast-local-type
-    TriggerEvent('HRLib:RemovedEntity', entityType, entity)
+
+    if not HRLib.table.find(trackedEntities, { entity = entity }) then
+        TriggerEvent('HRLib:RemovedEntity', entityType, entity)
+    end
+
     deleteEntity(entity)
 end
 
@@ -31,90 +35,107 @@ DeleteObject = function(object)
     DeleteEntity(object)
 end
 
----Function to track an entity's deletion with given entityId.\
----It is important to understand that a delay of 100ms may exist between the event execution and the entity's deletion (it's necessary for the optimization)
----@param entityId integer
----@param attachedInfo table<string, any>?
----@param cb fun(entityType: 'vehicle'|'ped'|'object')? The callback function to trigger while the entity is still existing (if you want to check something each time for the entity you should use function)
-HRLib.TrackEntity = function(entityId, attachedInfo, cb)
-    if not DoesEntityExist(entityId) then return end
+if not IsDuplicityVersion() then
+    ---Function to track an entity's deletion with given entityId.\
+    ---It is important to understand that a delay of 100ms may exist between the event execution and the entity's deletion (it's necessary for the optimization)
+    ---@param entityId integer
+    ---@param attachedInfo table<string, any>?
+    ---@param cb fun(entityType: 'vehicle'|'ped'|'object')? The callback function to trigger while the entity is still existing (if you want to check something each time for the entity you should use function)
+    HRLib.TrackEntity = function(entityId, attachedInfo, cb)
+        if not DoesEntityExist(entityId) then return end
 
-    local entityType = GetEntityType(entityId)
-    entityType = entityType == 1 and 'ped' or entityType == 2 and 'vehicle' or entityType == 3 and 'object' ---@diagnostic disable-line: cast-local-type
-    CreateThread(function(threadId)
-        local currIndex <const>, properties = #trackedEntities+1, nil
-        trackedEntities[currIndex] = { threadId = threadId, entity = entityId }
+        local entityType = GetEntityType(entityId)
+        entityType = entityType == 1 and 'ped' or entityType == 2 and 'vehicle' or entityType == 3 and 'object' ---@diagnostic disable-line: cast-local-type
+        CreateThread(function(threadId)
+            local currIndex <const>, properties = #trackedEntities+1, nil
+            trackedEntities[currIndex] = { threadId = threadId, entity = entityId }
 
-        while trackedEntities[currIndex] do
-            if not DoesEntityExist(entityId) then
-                TriggerEvent('HRLib:TrackedEntityDeleted', entityType, entityId, properties)
+            while trackedEntities[currIndex] do
+                if not DoesEntityExist(entityId) then
+                    TriggerEvent('HRLib:TrackedEntityDeleted', entityType, entityId, properties)
 
-                trackedEntities[currIndex] = nil
-            elseif not IsDuplicityVersion() then
-                local state <const> = HRLib.table.cloneStateBag(('%s:%s'):format((entityType == 'ped' and IsPedAPlayer(entityId)) and 'player' or 'entity', (entityType == 'ped' and IsPedAPlayer(entityId)) and GetPlayerServerId(NetworkGetPlayerIndexFromPed(entityId)) or NetworkGetNetworkIdFromEntity(entityId)))
-                if entityType == 'vehicle' then
-                    properties = HRLib.GetVehicleProperties(entityId)
-                    properties.state = state ---@diagnostic disable-line: inject-field
-                elseif entityType == 'ped' then
-                    properties = {
-                        pedModel = GetEntityModel(entityId),
-                        pedType = GetPedType(entityId),
-                        isPlayer = IsPedAPlayer(entityId),
-                        pedMoney = GetPedMoney(entityId),
-                        lastCoords = GetEntityCoords(entityId),
-                        isFreezed = IsEntityPositionFrozen(entityId),
-                        state = state
-                    }
-                elseif entityType == 'object' then
-                    properties = {
-                        objectModel = GetEntityModel(entityId),
-                        isObjectLocal = NetworkGetEntityIsLocal(entityId),
-                        lastCoords = GetEntityCoords(entityId),
-                        isFreezed = IsEntityPositionFrozen(entityId),
-                        state = state
-                    }
-                end
-
-                if type(attachedInfo) == 'table' and table.type(attachedInfo) == 'hash' then
-                    for k,v in pairs(attachedInfo) do
-                        properties[k] = v
+                    trackedEntities[currIndex] = nil
+                elseif not IsDuplicityVersion() then
+                    local state <const> = HRLib.table.cloneStateBag(('%s:%s'):format((entityType == 'ped' and IsPedAPlayer(entityId)) and 'player' or 'entity', (entityType == 'ped' and IsPedAPlayer(entityId)) and GetPlayerServerId(NetworkGetPlayerIndexFromPed(entityId)) or NetworkGetNetworkIdFromEntity(entityId)))
+                    if entityType == 'vehicle' then
+                        properties = HRLib.GetVehicleProperties(entityId)
+                        properties.state = state ---@diagnostic disable-line: inject-field
+                    elseif entityType == 'ped' then
+                        properties = {
+                            pedModel = GetEntityModel(entityId),
+                            pedType = GetPedType(entityId),
+                            isPlayer = IsPedAPlayer(entityId),
+                            pedMoney = GetPedMoney(entityId),
+                            lastCoords = GetEntityCoords(entityId),
+                            isFreezed = IsEntityPositionFrozen(entityId),
+                            state = state
+                        }
+                    elseif entityType == 'object' then
+                        properties = {
+                            objectModel = GetEntityModel(entityId),
+                            isObjectLocal = NetworkGetEntityIsLocal(entityId),
+                            lastCoords = GetEntityCoords(entityId),
+                            isFreezed = IsEntityPositionFrozen(entityId),
+                            state = state
+                        }
                     end
+
+                    if type(attachedInfo) == 'table' and table.type(attachedInfo) == 'hash' then
+                        for k,v in pairs(attachedInfo) do
+                            properties[k] = v
+                        end
+                    end
+
+                    if type(cb) == 'function' or type(cb) == 'table' and cb['__cfx_functionReference'] then
+                        local result <const> = cb(entityType --[[@as 'vehicle'|'ped'|'object' ]])
+                        if type(result) == 'table' and table.type(result) ~= 'empty' then
+                            for k,v in pairs(result) do
+                                properties[k] = v
+                            end
+                        end
+                    end
+
+                    trackedEntities[currIndex].properties = properties
                 end
 
-                trackedEntities[currIndex].properties = properties
-
-                if type(cb) == 'function' or type(cb) == 'table' and cb['__cfx_functionReference'] then
-                    cb(entityType --[[@as 'vehicle'|'ped'|'object' ]])
-                end
+                Wait(100)
             end
-
-            Wait(100)
-        end
-    end)
-end
-
----@param entity integer
----@param index string? if nil, it will return all current properties
----@return any?
-HRLib.GetValueFromTrackedEntityProperties = function(entity, index)
-    local value
-
-    HRLib.table.focusedArray(trackedEntities, { entity = entity }, function(i, curr)
-        if index and curr.properties[index] then
-            value = curr.properties[index] ---@diagnostic disable-line: redundant-return-value
-        elseif index == nil then
-            value = curr.properties
-        end
-    end)
-
-    return value
-end
-
----Remove the tracker from the entity, created from HRLib.TrackEntity
----@param entityId integer
-HRLib.RemoveEntityTracker = function(entityId)
-    local found <const>, index <const> = HRLib.table.find(trackedEntities, { entity = entityId }, true)
-    if found then
-        trackedEntities[index --[[@as integer]]] = nil
+        end)
     end
+
+    ---@param entityId integer
+    ---@return boolean
+    HRLib.GetIsEntityTracked = function(entityId)
+        return HRLib.table.find(trackedEntities, { entity = entityId })
+    end
+
+    ---@param entity integer
+    ---@param index string? if nil, it will return all current properties
+    ---@return any?
+    HRLib.GetTrackedEntityProperties = function(entity, index)
+        local value
+
+        HRLib.table.focusedArray(trackedEntities, { entity = entity }, function(i, curr)
+            if index and curr.properties[index] then
+                value = curr.properties[index] ---@diagnostic disable-line: redundant-return-value
+            elseif index == nil then
+                value = curr.properties
+            end
+        end)
+
+        return value
+    end
+
+    ---Remove the tracker from the entity, created from HRLib.TrackEntity
+    ---@param entityId integer
+    HRLib.RemoveEntityTracker = function(entityId)
+        local found <const>, index <const> = HRLib.table.find(trackedEntities, { entity = entityId }, true)
+        if found then
+            trackedEntities[index --[[@as integer]]] = nil
+        end
+    end
+
+    RegisterNetEvent('HRLib:TrackedEntityDeleted')
 end
+
+RegisterNetEvent('HRLib:RemovedEntity')
