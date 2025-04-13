7c3cea74c798124a42db388b14b7c10711920f5b
HR0110 <hrisiraya@abv.bg>
2025-04-13 11:39:31 +0300

    fix(modules/FPlayer): fixed FPlayer's metatable andmade documentational changes

--- modules/modules/fplayer.lua
+++ modules/modules/fplayer.lua
@@ -6,12 +6,15 @@ local fplayer <const> = setmetatable({
         if not self.id then
             return 'FPlayer Id\'s not set'
         else
-            return self[key]
+            return rawget(self, key)
         end
     end
 })
 
+---@param id integer
 function fplayer:newObject(id)
+    if not HRLib.DoesIdExist(tonumber(id)) then return end
+
     local newObject <const> = HRLib.table.deepclone(fplayer)
     newObject.id = id
 
@@ -22,19 +25,19 @@ end
 function fplayer:Teleport(coords)
     if type(coords) ~= 'vector3' then return end
 
-    SetEntityCoordsNoOffset(GetPlayerPed(GetPlayerFromServerId(self.id)), coords) ---@diagnostic disable-line: missing-parameter, param-type-mismatch
+    SetEntityCoordsNoOffset(GetPlayerPed(isServer and self.id or GetPlayerFromServerId(self.id)), coords) ---@diagnostic disable-line: missing-parameter, param-type-mismatch
 end
 
 if isServer then
     ---@param vehModel string|integer vehicle model
     ---@param spawnPlayerInside boolean? spawn ped inside the vehicle
     ---@param saveVehicle boolean? save the old vehicle
-    ---@return integer
+    ---@return integer? vehicle
     function fplayer:SpawnVehicle(vehModel, spawnPlayerInside, saveVehicle)
         local ped <const> = GetPlayerPed(self.id)
 
         if not saveVehicle then
-            local oldVeh = GetVehiclePedIsIn(ped, false)
+            local oldVeh <const> = GetVehiclePedIsIn(ped, false)
             if oldVeh then
                 DeleteEntity(oldVeh)
             end
@@ -49,40 +52,55 @@ if isServer then
         return veh
     end
 
-    ---@param msg string content of the notice
-    ---@param type string notice type ( success ; error ; info ; warning )
-    ---@param time number time until notice closes (in miliseconds)
-    function fplayer:Notify(msg, type, time)
-        HRLib.Notify(self.id, msg, type, time)
+    ---@param description string content of the notice
+    ---@param type 'success'|'error'|'info'|'warning' notification type
+    ---@param duration number? notification duration in msec, default is 2500
+    ---@param pos 'top-right'|'center-right'|'bottom-right'|'frombelow-right'|'top-left'|'left-center'|'frombelow-left'?
+    ---@param sound boolean? default is true
+    function fplayer:Notify(description, type, duration, pos, sound)
+        HRLib.Notify(self.id, description, type, duration, pos, sound)
     end
 
-    ---@return HRLibServerFPlayer[]|{}
+    ---@param eventName any the event name
+    ---@param ... any the event parameters
+    function fplayer:FocusedEvent(eventName, ...)
+        if type(eventName) ~= 'string' then return end
+
+        TriggerClientEvent(eventName, self.id, ...)
+    end
+
+    ---@return HRLibServerFPlayer[]?
     HRLib.AllFPlayers = function()
         local allFPlayers <const> = {}
 
         local players <const> = GetPlayers()
         for i=1, #players do
-            allFPlayers[#allFPlayers+1] = fplayer:newObject(tonumber(players[i]))
+            allFPlayers[#allFPlayers+1] = fplayer:newObject(tonumber(players[i]) --[[@as integer]])
         end
 
-        return allFPlayers
+        return #allFPlayers > 0 and allFPlayers or nil
+    end
+
+    ---Function to get FPlayer's list of methods
+    ---@param playerId integer?
+    ---@return HRLibServerFPlayer?
+    HRLib.GetFPlayer = function(playerId)
+        if playerId and HRLib.DoesIdExist(playerId) then
+            return fplayer:newObject(playerId)
+        end
     end
 else
     ---@param vehModel string|integer vehicle spawn code or model hash
     ---@param spawnPlayerInside boolean?
     ---@param saveVehicle boolean?
-    ---@return integer?
+    ---@return integer? vehicle
     function fplayer:SpawnVehicle(vehModel, spawnPlayerInside, saveVehicle)
         local model <const>, ped <const> = joaat(vehModel), GetPlayerPed(GetPlayerFromServerId(self.id))
 
-        RequestModel(model)
-
-        while not HasModelLoaded(model) do
-            Wait(50)
-        end
+        HRLib.RequestModel(model)
 
         if not saveVehicle then
-            local oldVeh = GetVehiclePedIsIn(ped, false)
+            local oldVeh <const> = GetVehiclePedIsIn(ped, false)
             if oldVeh then
                 DeleteEntity(oldVeh)
             end
@@ -109,25 +127,21 @@ else
         SetEntityCanBeDamaged(ped, not toggle)
         SetPlayerControl(GetPlayerFromServerId(self.id), not toggle) ---@diagnostic disable-line: missing-parameter
     end
+
+    ---Function to get FPlayer's list of methods
+    ---@return HRLibClientFPlayer?
+    HRLib.GetFPlayer = function()
+        return fplayer:newObject(GetPlayerServerId(PlayerId()))
+    end
 end
 
----@param health number value of the player health
+---@param health number? value of the player health
 function fplayer:SetHealth(health)
-    local ped <const> = GetPlayerPed(GetPlayerFromServerId(self.id))
-    SetEntityHealth(ped, health or (IsPedMale(ped) and 200 or 100))
+    local ped <const> = GetPlayerPed(isServer and self.id or GetPlayerFromServerId(self.id))
+    SetEntityHealth(ped, type(health) == 'number' and health or GetEntityMaxHealth(ped))
 end
 
 ---@param toggle boolean? toggle player invincible
 function fplayer:SetInvincibility(toggle)
-    SetPlayerInvincible(GetPlayerFromServerId(self.id), toggle or false)
-end
-
----@param playerId integer?
----@return HRLibClientFPlayer?
-HRLib.GetFPlayer = function(playerId)
-    if playerId and HRLib.DoesIdExist(playerId) then
-        return fplayer:newObject(playerId)
-    elseif not playerId then
-        return fplayer:newObject(GetPlayerServerId(PlayerId()))
-    end
+    SetPlayerInvincible(isServer and self.id or GetPlayerFromServerId(self.id), toggle or false)
 end
