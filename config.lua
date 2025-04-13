9c5da1f6fa9b93cbce3d8f831153ad4054ee2e00
HR0110 <hrisiraya@abv.bg>
2025-04-13 11:34:56 +0300

    feat(config): added new configuration for the default progress bar's position and the debug command

--- config.lua
+++ config.lua
@@ -4,6 +4,8 @@ config.defaultWebHook = 'https://discord.com/api/webhooks/********************' 
 
 config.defaultNotificationsPosition = 'left-center' -- available positions - 'top-right', 'center-right', 'bottom-right', 'frombelow-right', 'top-left', 'left-center', 'frombelow-left'
 
+config.defaultProgressBarPosition = 'bottom-center' -- available positions - 'center-left', 'center', 'center-right', 'bottom-left', 'bottom-center', 'bottom-right'
+
 config.alertDialogueTranslation = {
     agreeButton = 'Agree',
     cancelButton = 'Cancel'
@@ -12,6 +14,22 @@ config.alertDialogueTranslation = {
 config.inputDialogueTranslation = {
     confirmButton = 'Confirm',
     cancelButton = 'Cancel'
+}
+
+config.debug = {
+    enable = true,
+    commandName = 'hrlib_debug',
+    commandHelp = 'Debug command of HRLib',
+    args = {
+        type = {
+            name = 'functionType',
+            help = 'Available types: showTextUI, isTextUIOpen, hideTextUI, progressBar, alertDialogue, inputDialogue, notify'
+        },
+        secondParameter = {
+            name = '...',
+            help = 'it changes according to the chosen function type'
+        }
+    }
 }
 
 return config
