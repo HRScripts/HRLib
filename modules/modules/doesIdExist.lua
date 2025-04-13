f64f276357fa50235fabded1d05ed43fa07d4952
HR0110 <hrisiraya@abv.bg>
2025-04-13 11:39:07 +0300

    docs(modules/doesIdExist): documentational fixes and changes

--- modules/modules/doesIdExist.lua
+++ modules/modules/doesIdExist.lua
@@ -1,11 +1,11 @@
----@param playerId integer? player server Id
+---@param playerId integer|string? player server Id
 ---@return boolean
 HRLib.DoesIdExist = function(playerId)
     if playerId == nil or type(playerId) ~= 'number' or type(playerId) == 'string' and type(tonumber(playerId)) ~= 'number' then return false end
 
     playerId = tonumber(playerId) --[[@as integer]]
 
-    local players <const> = IsDuplicityVersion() and GetPlayers() or HRLib.GetPlayers()
+    local players <const> = IsDuplicityVersion() and GetPlayers() or HRLib.GetPlayers() --[[@as string[] ]]
     for i=1, #players do
         if tonumber(players[i]) == playerId then
             return true
