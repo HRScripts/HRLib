728352dbb919c5f4a2523e3fda5706552558f7c4
HR0110 <hrisiraya@abv.bg>
2025-06-05 00:11:40 +0300

    fix(modules/doesIdExist): fixed DoesIdExist logic so it can work properly on the server side

--- modules/modules/doesIdExist.lua
+++ modules/modules/doesIdExist.lua
@@ -1,15 +1,16 @@
 ---@param playerId integer|string? player server Id
+---@param onlyIdsOfJoinedPlayers boolean? default: false. Sets whether or not the check must be over the ids of the joined/spawned players in the game. Available in server side only
 ---@return boolean
-HRLib.DoesIdExist = function(playerId)
+HRLib.DoesIdExist = function(playerId, onlyIdsOfJoinedPlayers)
     if playerId == nil or type(playerId) ~= 'number' or type(playerId) == 'string' and type(tonumber(playerId)) ~= 'number' then return false end
 
     playerId = tonumber(playerId) --[[@as integer]]
 
-    local players <const> = IsDuplicityVersion() and GetPlayers() or HRLib.GetPlayers() --[[@as string[] ]]
-    for i=1, #players do
-        if tonumber(players[i]) == playerId then
-            return true
-        end
+    local isServer <const> = IsDuplicityVersion()
+    if HRLib.table.find(isServer and GetPlayers() or HRLib.GetPlayers() --[[@as table]], isServer and tostring(playerId) or playerId) then
+        return true
+    elseif not onlyIdsOfJoinedPlayers and isServer and GetPlayerName(playerId) then
+        return true
     end
 
     return false
