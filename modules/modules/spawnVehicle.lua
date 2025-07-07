c9df19f6553ce0a581a64593ca6deff894e8efed
HR0110 <hrisiraya@abv.bg>
2025-07-07 13:42:08 +0300

    fix(modules/spawnVehicle): fixed the spawn vehicle logic in server side

--- modules/modules/spawnVehicle.lua
+++ modules/modules/spawnVehicle.lua
@@ -16,5 +16,40 @@ HRLib.SpawnVehicle = function(vehModel, coords)
         end
     end
 
+    if isServer then
+        if #GetPlayers() == 0 then return end
+
+        return CreateVehicleServerSetter(modelHash, HRLib.ClientCallback('____g_hr_spvgh_c_mdl', HRLib.ClosestIPlayer(coords, 500, false).id, vehModel), coords) ---@diagnostic disable-line: missing-parameter, param-type-mismatch
+    end
+
     return CreateVehicle(modelHash, coords, true, true) ---@diagnostic disable-line: missing-parameter, param-type-mismatch
+end
+
+if not isServer then
+    HRLib.CreateCallback('____g_hr_spvgh_c_mdl', true, function(model)
+        model = type(model) == 'string' and joaat(model) or model
+        if not IsModelValid(model) or not IsModelAVehicle(model) then return end
+
+        local found
+
+        local prefix, modules = 'IsThisModelA%s', { 'Bicycle', 'Bike', 'Boat', 'Car', 'Heli', 'Jetski', 'Plane', 'Quadbike', 'Train' }
+        for i=1, #modules do
+            if _ENV[(prefix):format(modules[i])](model) then
+                found = modules[i]
+
+                break
+            end
+        end
+
+        found = found == 'Bicycle' and 'automobile' or found
+        found = found == 'Bike' and 'bike' or found
+        found = found == 'Boat' and 'boat' or found
+        found = found == 'Car' and 'automobile' or found
+        found = found == 'Heli' and 'heli' or found
+        found = found == 'Plane' and 'plane' or found
+        found = found == 'Quadbike' and 'bike' or found
+        found = found == 'Train' and 'train' or found
+
+        return found
+    end)
 end
