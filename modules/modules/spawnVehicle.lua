3227cd3ca69b77bfe03181cbc4727d501a67455a
HR0110 <hrisiraya@abv.bg>
2025-11-12 22:09:23 +0200

    fix(modules/spawnVehicle): fixed spawnVehicle for the server side and for the client side

--- modules/modules/spawnVehicle.lua
+++ modules/modules/spawnVehicle.lua
@@ -1,31 +1,26 @@
 local isServer <const> = IsDuplicityVersion()
 
----@param vehModel string|integer existing vehicle model or model hash
----@param coords vector4 vehicle spawn coordinates
----@return integer? vehicle
-HRLib.SpawnVehicle = function(vehModel, coords)
-    local modelHash <const> = type(vehModel) == 'number' and vehModel or joaat(vehModel)
-
-    if type(coords) ~= 'vector4' or (isServer and type(modelHash) ~= 'number' or (not isServer and not IsModelValid(modelHash))) then return end
-
-    if not isServer then
-        RequestModel(modelHash)
-
-        while not HasModelLoaded(modelHash) do
-            Wait(10)
+if isServer then
+    ---Function to spawn a vehicle from the server side
+    ---@param vehModel string|integer existing vehicle model or model hash
+    ---@param coords vector4 vehicle spawn coordinates
+    ---@param vehType string? Required parameter which's only not required when there is more than one player in the server
+    ---@return integer? vehicle
+    HRLib.SpawnVehicle = function(vehModel, coords, vehType)
+        do
+            local currRes <const> = GetInvokingResource() or GetCurrentResourceName()
+            if type(coords) ~= 'vector4' then
+                return warn(('Resource %s tried to spawn a vehicle without providing spawn coords'):format(currRes))
+            elseif not vehType and #GetPlayers() == 0 then
+                return warn(('Resource %s tried to spawn a vehicle without providing vehicle type while there are no players in the server!'):format(currRes))
+            elseif not vehType then
+                vehType = HRLib.ClientCallback('____g_hr_spvgh_c_mdl', HRLib.ClosestIPlayer(coords, 500, false)?.id or GetPlayers()[1], vehModel)
+            end
         end
+
+        return CreateVehicleServerSetter(type(vehModel) == 'number' and vehModel or joaat(vehModel), vehType, table.unpack(coords))
     end
-
-    if isServer then
-        if #GetPlayers() == 0 then return end
-
-        return CreateVehicleServerSetter(modelHash, HRLib.ClientCallback('____g_hr_spvgh_c_mdl', HRLib.ClosestIPlayer(coords, 500, false).id, vehModel), coords) ---@diagnostic disable-line: missing-parameter, param-type-mismatch
-    end
-
-    return CreateVehicle(modelHash, coords, true, true) ---@diagnostic disable-line: missing-parameter, param-type-mismatch
-end
-
-if not isServer then
+else
     HRLib.CreateCallback('____g_hr_spvgh_c_mdl', true, function(model)
         model = type(model) == 'string' and joaat(model) or model
         if not IsModelValid(model) or not IsModelAVehicle(model) then return end
@@ -52,4 +47,17 @@ if not isServer then
 
         return found
     end)
+
+    ---@param vehModel string|integer existing vehicle model or model hash
+    ---@param coords vector4 vehicle spawn coordinates
+    ---@return integer? vehicle
+    HRLib.SpawnVehicle = function(vehModel, coords)
+        local modelHash <const> = type(vehModel) == 'number' and vehModel or joaat(vehModel)
+
+        if not IsModelValid(modelHash) or type(coords) ~= 'vector4' then return end
+
+        HRLib.RequestModel(vehModel)
+
+        return CreateVehicle(modelHash, coords.x, coords.y, coords.z, coords.w, true, true)
+    end
 end
