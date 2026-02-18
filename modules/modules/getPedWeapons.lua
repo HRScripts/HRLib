0159ae3dac71ed8a97ae4f16cfd4424422555690
HR0110 <hrisiraya@abv.bg>
2026-02-18 20:25:45 +0200

    feat(modules/getPedWeapons): added HRLib.GetPedWeapons for the server side

--- modules/modules/getPedWeapons.lua
+++ modules/modules/getPedWeapons.lua
@@ -1,15 +1,29 @@
-if IsDuplicityVersion() then return end
+if IsDuplicityVersion() then
+    ---@param playerId integer
+    ---@return string[]
+    HRLib.GetPedWeapons = function(playerId)
+        if not HRLib.DoesIdExist(playerId, true) then error('^4HRLib:^1 This resource tried to execute HRLib.GetPedWeapons, providing an invalid playerId.\nPlease make sure to always check the id\'s existance before using this function.') end
 
----@return string[]?
-HRLib.GetPedWeapons = function()
-    local pedWeapons <const> = {}
+        return HRLib.ClientCallback('__HRLib_getPedWeapons', playerId)
+    end
+else
+    ---@return string[]?
+    HRLib.GetPedWeapons = function()
+        local pedWeapons <const> = {}
 
-    local playerPed <const> = PlayerPedId()
-    for i=1, #HRLib.Weapons do
-        if HasPedGotWeapon(playerPed, joaat(HRLib.Weapons[i]), false) then
-            pedWeapons[#pedWeapons+1] = HRLib.Weapons[i]
+        local playerPed <const> = PlayerPedId()
+        for i=1, #HRLib.Weapons do
+            if HasPedGotWeapon(playerPed, joaat(HRLib.Weapons[i]), false) then
+                pedWeapons[#pedWeapons+1] = HRLib.Weapons[i]
+            end
         end
+
+        return #pedWeapons > 0 and pedWeapons or nil
     end
 
-    return #pedWeapons > 0 and pedWeapons or nil
+    HRLib.CreateCallback('__HRLib_getPedWeapons', true, function()
+        if TriggerSide == 'server' then
+            return HRLib.GetPedWeapons()
+        end
+    end)
 end
